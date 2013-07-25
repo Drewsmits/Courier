f944891e0c328946c8a2cd00518cf76fb64e6a81
Andrew B. Smith <drewsmits@gmail.com>
2013-07-25 07:26:36 -0700

    Added courier server

---
+++ CourierServer/target/scala-2.10/src_managed/main/controllers/routes.java
@@ -0,0 +1,19 @@
+// @SOURCE:/Users/andrewsmith/Workspace/Courier/CourierServer/conf/routes
+// @HASH:01d721dfdccb01b4b1cf6ffa7f4dd4c7b7198f40
+// @DATE:Fri Mar 29 11:42:10 PDT 2013
+
+package controllers;
+
+public class routes {
+public static final controllers.ReverseApplication Application = new controllers.ReverseApplication();
+public static final controllers.ReverseAssets Assets = new controllers.ReverseAssets();
+public static class javascript {
+public static final controllers.javascript.ReverseApplication Application = new controllers.javascript.ReverseApplication();
+public static final controllers.javascript.ReverseAssets Assets = new controllers.javascript.ReverseAssets();    
+}   
+public static class ref {
+public static final controllers.ref.ReverseApplication Application = new controllers.ref.ReverseApplication();
+public static final controllers.ref.ReverseAssets Assets = new controllers.ref.ReverseAssets();    
+} 
+}
+              
